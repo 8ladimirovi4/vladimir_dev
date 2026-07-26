40669ac9a7da2296f0906226516c3a4283e75b2e
sea4836-systeme-ru <vladimir.leonov@systeme.ru>
2026-07-26 19:31:44 +0300

    chore: edit file paths; gitignore

--- src/shared/config/cv.ts
+++ src/shared/config/cv.ts
@@ -4,11 +4,11 @@ import type { Locale } from '@/shared/i18n';
 
 export const CV_FILES = {
   en: {
-    path: '/Vladimir_Leonov_Frontend_EN.pdf',
+    path: '/cv/Vladimir_Leonov_Frontend_EN.pdf',
     filename: 'Vladimir_Leonov_Frontend_EN.pdf',
   },
   ru: {
-    path: '/Vladimir_Leonov_Frontend_RU.pdf',
+    path: '/cv/Vladimir_Leonov_Frontend_RU.pdf',
     filename: 'Vladimir_Leonov_Frontend_RU.pdf',
   },
 } as const;
